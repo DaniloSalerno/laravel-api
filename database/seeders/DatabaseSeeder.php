6e483670a858d30d43cbbcbd4759cf180248a383
DaniloSalerno <salerno.danilo@hotmail.it>
2023-11-17 14:22:46 +0100

    add route for API

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,8 +25,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             ProjectSeeder::class,
-            TypeSeeder::class,
-            TechnologySeeder::class,
+            //TypeSeeder::class,
+            //TechnologySeeder::class,
         ]);
     }
 }
